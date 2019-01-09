f133c8733bb99ccbe75db9364aa02b0965b7a034
iyinraphael <iyinraphael@gmail.com>
2019-01-09 08:49:56 -0800

    use of CAKeyFrameAnimation to animate background color of view

--- Simple Animation/ViewController.swift
+++ Simple Animation/ViewController.swift
@@ -18,14 +18,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func Animatee(_ sender: Any) {
-        UIView.animate(withDuration: 0.5) {
-            
-            var frame = self.greenSquare.frame
-            frame.origin.x += 100
-            frame.origin.y += 200
-            
-            self.greenSquare.frame = frame
+        
+        //UIVIEW animate for simple aniamtion
+//        UIView.animate(withDuration: 0.5) {
+//            var frame = self.greenSquare.frame
+//            frame.origin.x += 100
+//            frame.origin.y += 200
+//
+//            self.greenSquare.frame = frame
+//            self.greenSquare.backgroundColor = .red
+        //}
+        
+        CATransaction.begin()
+        
+        CATransaction.setCompletionBlock {
+            self.greenSquare.layer.backgroundColor = UIColor.red.cgColor
         }
+        let animation = CAKeyframeAnimation(keyPath: "backgroundColor")
+        animation.values = [UIColor.black.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.red.cgColor, UIColor.black.cgColor]
+        animation.duration = 2.0
+        //animation.repeatCount = .infinity
+        greenSquare.layer.add(animation, forKey: "backgroundColorAnimation")
+        CATransaction.commit()
+    
     }
     
     let greenSquare = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
