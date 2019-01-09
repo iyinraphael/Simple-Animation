5ddfb546974cbd0edf0d7e95dc1d2f52c614dd1b
iyinraphael <iyinraphael@gmail.com>
2019-01-09 08:06:15 -0800

    initial commit

---
+++ Simple Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Animation
+//
+//  Created by Iyin Raphael on 1/9/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
